ecb410239c196ec799ca4dd61a946fb5edbadfbf
Dylan Anthony <dbanty@users.noreply.github.com>
2023-02-02 16:58:38 -0700

    feat: Add default CORs settings to allow Sandbox, print out Sandbox URL to CLI for easier access.

--- src/main/kotlin/com/example/template/Application.kt
+++ src/main/kotlin/com/example/template/Application.kt
@@ -1,10 +1,18 @@
 package com.example.template
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class Application
+class Application {
+    // Print out the Apollo Studio Sandbox URL for querying this application
+    @Bean
+    fun commandLineRunner() = CommandLineRunner {
+        println("Apollo Studio Sandbox URL: https://studio.apollographql.com/sandbox/explorer?endpoint=http://localhost:8080/graphql")
+    }
+}
 
 @Suppress("SpreadOperator")
 fun main(args: Array<String>) {
